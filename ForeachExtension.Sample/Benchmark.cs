001be817093722fe3da36a2fc65c8e4eb680681d
murat-boz <muratboz@hotmail.com>
2022-10-24 13:19:41 +0300

    Add project files.

---
+++ ForeachExtension.Sample/Benchmark.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ForeachExtension.Sample
+{
+    [MemoryDiagnoser(false)]
+    public class Benchmark
+    {
+        [Params(10, 100, 1000, 10000, 100000)]
+        public int Size { get; set; }
+
+        [Benchmark]
+        public void NormalForLoop()
+        {
+            for (int i = 0; i < this.Size; i++)
+            {
+                this.DoSomething();
+            }
+        }
+
+        [Benchmark]
+        public void ExtensionForLoop()
+        {
+            foreach (var i in 0..this.Size)
+            {
+                this.DoSomething();
+            }
+        }
+
+        private void DoSomething()
+        {
+            // Do Nothing
+        }
+    }
+}
---
+++ ForeachExtension/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ForeachExtension
+{
+    public static class Extensions
+    {
+        public static CustomIntEnumerator GetEnumerator(this Range range)
+        {
+            return new CustomIntEnumerator(range);
+        }
+
+        public static CustomIntEnumerator GetEnumerator(this int number)
+        {
+            return new CustomIntEnumerator(new Range(0, number));
+        }
+    }
+
+    public struct CustomIntEnumerator
+    {
+        private int current;
+        public int end;
+
+        public int Current => current;
+
+        public CustomIntEnumerator(Range range)
+        {
+            if (range.End.IsFromEnd)
+            {
+                throw new NotSupportedException();
+            }
+
+            this.current = range.Start.Value - 1;
+            this.end     = range.End.Value;
+        }
+
+        public bool MoveNext()
+        {
+            this.current++;
+
+            return this.current <= this.end;
+        }
+    }
+}
